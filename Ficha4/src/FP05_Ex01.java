72f5d4da288e1810873f3ee200e3edbb9b0bb6e9
joanapleonardo <joana.leonardo714@gmail.com>
2023-03-22 09:26:03 +0000

    inicio de vetores e matrizes

---
+++ Ficha4/src/FP05_Ex01.java
@@ -0,0 +1,25 @@
+//import obrigatório do package para fazer a leitura das introduções dos dados de utilizador
+import java.util.Scanner;
+
+class FP05_Ex01 {
+    public static void main(String[] args) {
+
+        System.out.println("\n***** Ficha Prática 05 - Exercício 01 *****\n");
+
+        //Instancia obrigatória da classe para fazer a leitura das introduções dos dados de utilizador
+        Scanner input = new Scanner(System.in);
+
+
+        //Declarar variáveis
+        int arr[] = new int [10];
+
+        for(int i<0; i==9; i++ ){
+            System.out.println([]);
+        }
+
+
+
+
+    }
+
+    }
